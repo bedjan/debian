6ad96a048a30a83a7d27117d52649d967686c8e9
bedjan <29821749+bedjan@users.noreply.github.com>
2018-03-05 19:02:11 +0100

    Add files via upload

--- skripty/debian_upgrade.sh
+++ skripty/debian_upgrade.sh
@@ -2,5 +2,5 @@
 
 #sakura -h -e "bash -c 'sudo apt-get update;wait; sudo apt list --upgradable -a;wait; sudo apt-get upgrade;wait;sudo apt autoremove; echo =====KONEC_UPGRADE=====' "
 #sleep 30 &
-sakura -h -e "bash -c 'echo ___Za_30s_se_spusti_UPGRADE___;sleep 20;sudo apt-get update;wait; sudo apt list --upgradable -a;wait; sudo apt-get -t stretch-backports dist-upgrade -y;wait;sudo apt autoremove;wait;sudo apt autoclean; echo =====KONEC_UPGRADE=====' " &
+sakura -h -e "bash -c 'echo ___Za_30s_se_spusti_UPGRADE___;sleep 30;sudo aptitude update;wait; sudo apt list --upgradable -a;wait; sudo aptitude -t stretch-backports dist-upgrade -y;wait;sudo apt autoremove;wait;sudo apt autoclean; echo =====KONEC_UPGRADE=====' " &
 
