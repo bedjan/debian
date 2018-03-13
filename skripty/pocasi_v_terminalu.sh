21c51251b74c3b01275b4616f35bd530b9d737b0
bedjan <29821749+bedjan@users.noreply.github.com>
2018-03-13 11:20:03 +0100

    Add files via upload

---
+++ skripty/pocasi_v_terminalu.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+xterm -hold  -max -fa 'Monospace' -fs 11 -e  bash -c "
+which curl && echo curl_nainstalovan
+echo pocasi_v_rychnov_nad_kneznou
+curl http://wttr.in/rychnov_nad_kneznou"
+
