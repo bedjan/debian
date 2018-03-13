ae8a8954c86ccaf93ac422d74e9476f5445862f4
bedjan <29821749+bedjan@users.noreply.github.com>
2018-03-13 11:45:31 +0100

    Add files via upload

--- skripty/pocasi_v_terminalu.sh
+++ skripty/pocasi_v_terminalu.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+#vice info na https://github.com/chubin/wttr.in
+#zobrazeni v jinem jazyce http://wttr.in/:translation
+
 xterm -hold  -max -fa 'Monospace' -fs 11 -e  bash -c "
 which curl && echo curl_nainstalovan
 echo pocasi_v_rychnov_nad_kneznou
-curl  -s  --location -H 'Accept-Language: cs'  http://wttr.in/"
+curl  -s  --location -H 'Accept-Language: cs'  wttr.in/rychnov_nad_kneznou?lang=cs"
 
+#curl wttr.in/rychnov_nad_kneznou?lang=cs
 #curl  -s  --location -H 'Accept-Language: cs'  http://wttr.in/
 
