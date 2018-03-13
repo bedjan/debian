f870e5c5d9fc0fb360bd61ce6546083df32ea711
bedjan <29821749+bedjan@users.noreply.github.com>
2018-03-13 14:30:13 +0100

    Add files via upload

--- skripty/pocasi_v_terminalu.sh
+++ skripty/pocasi_v_terminalu.sh
@@ -4,7 +4,7 @@
 # nebo sudo apt-get install ansiweather &&  ansiweather -l Prague,CZ -f 10
 
 
-xterm -hold  -max -fa 'Monospace' -fs 11 -e  bash -c "
+xterm -hold  -title POCASI -max -fa 'Monospace' -fs 11 -e  bash -c "
 which curl && echo curl_nainstalovan
 echo pocasi_v_rychnov_nad_kneznou
 curl  -s  --location -H 'Accept-Language: cs'  wttr.in/rychnov_nad_kneznou?lang=cs"
