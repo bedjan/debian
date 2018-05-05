c70c8db43062d41e9f72fd72703409d689ddf6e3
bedjan <29821749+bedjan@users.noreply.github.com>
2018-05-05 22:23:28 +0200

    Add files via upload

--- skripty/youtube_stazeni_videa.sh
+++ skripty/youtube_stazeni_videa.sh
@@ -1,2 +1,2 @@
 #!/bin/bash  
-urxvt -geometry 80x20+60+150 -title Youtube_stazeni_videa +hold -e bash -c "youtube-dl $(xsel --clipboard)"
+urxvt -geometry 80x20+60+150 -title Youtube_stazeni_videa +hold -e bash -c "youtube-dl --output '%(uploader)s%(title)s.%(ext)s' $(xsel --clipboard)"
