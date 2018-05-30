321fac0485d44d545a9a5e4adcbbe435c39a9d08
bedjan <29821749+bedjan@users.noreply.github.com>
2018-05-30 09:57:59 +0200

    Add files via upload

--- skripty/radia.sh
+++ skripty/radia.sh
@@ -1,4 +1,4 @@
 #!/bin/bash  
-wget -N https://gist.githubusercontent.com/bedjan/4ce6c5f78e7d780e94d7d6dedfd66337/raw/b293c8866329666115e5c80f51c6287eb3b9f61c/radia_online.m3u
 #qmmp /home/d/radia_online.m3u
+wget -N https://raw.githubusercontent.com/bedjan/mm/master/radia_online.m3u
 vlc radia_online.m3u
