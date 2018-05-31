199dcfec75a8937d9c90fa934b1c0f1add9c4263
bedjan <29821749+bedjan@users.noreply.github.com>
2018-05-31 12:11:38 +0200

    Add files via upload

--- skripty/radia.sh
+++ skripty/radia.sh
@@ -3,4 +3,4 @@
 wget -N https://raw.githubusercontent.com/bedjan/mm/master/radia_online.m3u
 #vlc radia_online.m3u
 
-xterm  -fa 'Monospace' -fs 14 -geometry 80x20+60+150 -title CZ_SK_radia  -e bash -c "vlc -I ncurses  https://raw.githubusercontent.com/bedjan/mm/master/radia_online.m3u"
+xterm  -fa 'Monospace' -fs 14 -geometry 30x70-25-25 -title CZ_SK_radia  -e bash -c "vlc -I ncurses  https://raw.githubusercontent.com/bedjan/mm/master/radia_online.m3u"
--- skripty/radia_anglicka.sh
+++ skripty/radia_anglicka.sh
@@ -3,4 +3,4 @@
 wget -N https://github.com/bedjan/mm/raw/master/english_radia.m3u
 #vlc english_radia.m3u
 
-xterm  -fa 'Monospace' -fs 14 -geometry 80x20+60+150 -title Anglicka_radia -e bash -c "vlc -I ncurses  https://raw.githubusercontent.com/bedjan/mm/master/english_radia.m3u"
+xterm  -fa 'Monospace' -fs 14 -geometry 30x70-25-25  -title Anglicka_radia -e bash -c "vlc -I ncurses  https://raw.githubusercontent.com/bedjan/mm/master/english_radia.m3u"
