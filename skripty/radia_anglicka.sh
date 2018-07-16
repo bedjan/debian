ac94871e6280669f60e7167462f77e8c58dfbd42
bedjan <29821749+bedjan@users.noreply.github.com>
2018-07-16 19:26:59 +0200

    Update radia_anglicka.sh

--- skripty/radia_anglicka.sh
+++ skripty/radia_anglicka.sh
@@ -1,6 +1,7 @@
 #!/bin/bash  
 #qmmp /home/d/english_radia.m3u
+$(echo ~)/
 wget -N https://github.com/bedjan/mm/raw/master/english_radia.m3u
-#vlc english_radia.m3u
+#vlc $(echo ~)/english_radia.m3u
 
 xterm  -fa 'Monospace' -fs 14 -geometry 30x70-25-25  -title Anglicka_radia -e bash -c "vlc -I ncurses  https://raw.githubusercontent.com/bedjan/mm/master/english_radia.m3u"
