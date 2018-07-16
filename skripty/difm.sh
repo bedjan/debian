18023601c45267b674b9ac59f6585f038a110efc
bedjan <29821749+bedjan@users.noreply.github.com>
2018-07-16 19:24:45 +0200

    Update difm.sh

--- skripty/difm.sh
+++ skripty/difm.sh
@@ -1,3 +1,4 @@
 #!/bin/bash  
+cd $(echo ~)/
 wget -N https://gist.github.com/bedjan/11917d14b9209035c1aa21815c959837/raw/fd21daa89c2784404d465b2da8435f807142c4ad/difm.m3u
-vlc difm.m3u
+vlc $(echo ~)/difm.m3u
