e0e4ff70bd114b79931979a85e2b44e685d4d539
bedjan <29821749+bedjan@users.noreply.github.com>
2018-08-05 08:28:50 +0200

    Update youtube_dl_upgrade.sh

--- skripty/youtube_dl_upgrade.sh
+++ skripty/youtube_dl_upgrade.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-urxvt -geometry 80x20+60+150 -title Youtube_dl_upgrade +hold -e bash -c "sudo apt-get install python-setuptools;sudo easy_install pip;sudo pip install --upgrade youtube-dl"
+xterm -geometry 80x20+60+150 -title Youtube_dl_upgrade +hold -e bash -c "sudo apt-get install python-setuptools;sudo easy_install pip;sudo pip install --upgrade pip youtube-dl"
