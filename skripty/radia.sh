005f240a5d6de5ca00bbd90973f023ec78fc21a0
bedjan <29821749+bedjan@users.noreply.github.com>
2018-08-13 15:21:01 +0200

    Delete radia.sh

--- skripty/radia.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash  
-#qmmp /home/d/radia_online.m3u
-cd $(echo ~)/
-wget -N https://raw.githubusercontent.com/bedjan/mm/master/radia_online.m3u & vlc $(echo ~)/radia_online.m3u
-
-#xterm  -fa 'Monospace' -fs 14 -geometry 30x70-25-25 -title CZ_SK_radia  -e bash -c "vlc -I ncurses  https://raw.githubusercontent.com/bedjan/mm/master/radia_online.m3u"
