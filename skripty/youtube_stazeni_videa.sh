ab1a5dbb98643555f2fff8c90a78d96d8f616026
bedjan <29821749+bedjan@users.noreply.github.com>
2018-08-13 15:20:09 +0200

    Delete youtube_stazeni_videa.sh

--- skripty/youtube_stazeni_videa.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash  
-urxvt -geometry 80x20+60+150 -title Youtube_stazeni_videa +hold -e bash -c "mkdir $(echo ~)/Videa;cd $(echo ~)/Videa;youtube-dl --output '%(title)s.%(ext)s' $(xsel --clipboard)"
