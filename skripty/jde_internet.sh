4fc95202e89ef93ac972b9f40c94b17077d516d6
bedjan <29821749+bedjan@users.noreply.github.com>
2018-08-13 15:19:18 +0200

    Delete jde_internet.sh

--- skripty/jde_internet.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash  
-
-sakura -h -e "bash -c 'echo ==test_pripojeni_internetu_musi_byt_===0%_packet_loss__==;wait;echo ===5_x_dotaz_na_IP_google===;wait;ping 8.8.8.8 -c 5;wait;echo ===5_x_dotaz_na_google.com===;wait;ping www.google.cz -c 5' "
