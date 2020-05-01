9f79c8c5369fe3124954229f7a94de877be4708e
bedjan <29821749+bedjan@users.noreply.github.com>
2020-05-01 18:31:11 +0200

    Add files via upload

---
+++ skripty/nemecko/radia.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+xterm -fa 'Monospace' -fs 14 -geometry 80x20+60+150 -title Online_radia -e bash -c "vlc -I ncurses https://raw.githubusercontent.com/bedjan/mm/master/radia_online.m3u"
+
---
+++ skripty/nemecko/translate.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#sudo apt-get install translate-shell
+
+##trans slovo
+##trans -shell -brief -b -p -no-auto
+
+# nebo manualne 
+#https://github.com/soimort/translate-shell
+
+# wget git.io/trans
+# chmod +x ./trans
